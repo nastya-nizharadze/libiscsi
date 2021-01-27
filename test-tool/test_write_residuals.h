969656fa3545fd1877c65c5cf5fcf1cbf5791b60
Anastasia Kovaleva <a.kovaleva@yadro.com>
2021-01-27 12:09:50 +0300

    test-tool: Refactoring residuals write tests
    Looking at test_write10_residuals.c, test_write12_residuals.c and
    test_write16_residuals.c tests the similarity of the testing scenario
    can be found. There are several EDTL and SPDTL combinations which are
    the same for different length write command tests. They form the core
    of testing scenario of these commands. There aren't so much differences
    in the way of testing these combinations itself either. It is possible
    to move the main parameters describing the testing scenario into a
    separate structure and move the scenario itself into a separate function.
    It will increase the readability and reduce the duplicate code of these tests.

---
+++ test-tool/test_write_residuals.h
@@ -0,0 +1,36 @@
+/*
+   Copyright (C) 2013 by Ronnie Sahlberg <ronniesahlberg@gmail.com>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _ISCSI_TESTS_WRITE_RESIDUALS_H_
+#define _ISCSI_TESTS_WRITE_RESIDUALS_H_
+
+#include <stddef.h>
+
+struct residuals_test_data {
+  size_t cdb_size;
+  unsigned int xfer_len;
+  unsigned int buf_len;
+  unsigned int residuals_kind;
+  size_t residuals_amount;
+  bool check_overwrite;
+  const char *log_messages;
+};
+
+extern bool command_is_implemented;
+extern void write_residuals_test (const struct residuals_test_data *tdata);
+
+#endif /* _ISCSI_TESTS_WRITE_RESIDUALS_H_ */
